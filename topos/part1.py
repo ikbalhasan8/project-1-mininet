b9b1749df93ab683846b3b6f231c386722893ed1
ikbalhasan8 <159504256+ikbalhasan8@users.noreply.github.com>
2024-02-13 15:56:21 +0700

    Update 4 part1.py

--- topos/part1.py
+++ topos/part1.py
@@ -11,7 +11,13 @@ class part1_topo(Topo):
         pass
         switch1 = self.addSwitch('s1')
         host1 = self.addHost('h1')
-        self.addLink(switch1,host1)
+        host2 = self.addHost('h2')
+        host3 = self.addHost('h3')
+        host4 = self.addHost('h4')
+        self.addLink(host1,switch1)
+        self.addLink(host2,switch1)
+        self.addLink(host3,switch1)
+        self.addLink(host4,switch1)
 
 
 topos = {"part1": part1_topo}
