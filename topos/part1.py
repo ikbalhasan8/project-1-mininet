56cbf08a72454312819501ead4a1cd4efb253e55
ikbalhasan8 <159504256+ikbalhasan8@users.noreply.github.com>
2024-02-13 16:10:09 +0700

    Update part1.py

--- topos/part1.py
+++ topos/part1.py
@@ -1,12 +1,7 @@
-#!/usr/bin/python3
-
 from mininet.topo import Topo
-from mininet.net import Mininet
-from mininet.util import dumpNodeConnections
-from mininet.cli import CLI
 
-
-class part1_topo(Topo):
+class MyTopo( Topo ):
+    "Simple topology of six switches and six hosts"
 
     def __init__( self ):
         "Custom topology"
@@ -14,35 +9,17 @@ class part1_topo(Topo):
         # Initialize topology
         Topo.__init__( self )
 
-        switch1 = self.addSwitch('s1')
-        host1 = self.addHost('h1')
-        host2 = self.addHost('h2')
-        host3 = self.addHost('h3')
-        host4 = self.addHost('h4')
-        self.addLink(host1,switch1)
-        self.addLink(host2,switch1)
-        self.addLink(host3,switch1)
-        self.addLink(host4,switch1)
-        
-topos = { 'part1': ( lambda: part1_topo() ) }
+        # Add hosts and switches
+        host1 = self.addHost( 'h1' )
+        host2 = self.addHost( 'h2' )
+        host3 = self.addHost( 'h3' )
+        host4 = self.addHost( 'h4' )
+        switch1 = self.addSwitch( 's1' )
 
-    # def build(self):
-    #     pass
-    #     switch1 = self.addSwitch('s1')
-    #     host1 = self.addHost('h1')
-    #     host2 = self.addHost('h2')
-    #     host3 = self.addHost('h3')
-    #     host4 = self.addHost('h4')
-    #     self.addLink(host1,switch1)
-    #     self.addLink(host2,switch1)
-    #     self.addLink(host3,switch1)
-    #     self.addLink(host4,switch1)
+        # Add links between SW and Hosts
+        self.addLink( host1, switch1 )
+        self.addLink( host2, switch1 )
+        self.addLink( host3, switch1 )
+        self.addLink( host4, switch1 )
 
-# topos = {"part1": part1_topo}
-
-# if __name__ == "__main__":
-#     t = part1_topo()
-#     net = Mininet(topo=t)
-#     net.start()
-#     CLI(net)
-#     net.stop()
+topos = { 'mytopo': ( lambda: MyTopo() ) }
