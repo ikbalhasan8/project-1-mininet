824befed720883e9f6025416f785d2b0a0803451
ikbalhasan8 <159504256+ikbalhasan8@users.noreply.github.com>
2024-02-13 16:17:57 +0700

    Update part1.py

--- topos/part1.py
+++ topos/part1.py
@@ -6,18 +6,19 @@ from mininet.util import dumpNodeConnections
 from mininet.cli import CLI
 
 
-class MyTopo(Topo):
-    def __init__(self):
-        Topo.__init__(self)
-        s1 = self.addSwitch('s1')
-        h1 = self.addHost('h1')
-        h2 = self.addHost('h2')
-        h3 = self.addHost('h3')
-        h4 = self.addHost('h4')
-        self.addLink(h1,s1)
-        self.addLink(h2,s1)
-        self.addLink(h3,s1)
-        self.addLink(h4,s1)
+class part1_topo(Topo):
+    def build(self):
+        pass
+        switch1 = self.addSwitch('s1')
+        host1 = self.addHost('h1')
+        self.addLink(host1,switch1)
 
 
-topos = { 'mytopo': ( lambda: MyTopo() ) }
+topos = {"part1": part1_topo}
+
+if __name__ == "__main__":
+    t = part1_topo()
+    net = Mininet(topo=t)
+    net.start()
+    CLI(net)
+    net.stop()
