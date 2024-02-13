fec354735ab6977b807bcee453860cd65dd84fc4
ikbalhasan8 <159504256+ikbalhasan8@users.noreply.github.com>
2024-02-13 16:06:41 +0700

    Update part1.py

--- topos/part1.py
+++ topos/part1.py
@@ -7,8 +7,13 @@ from mininet.cli import CLI
 
 
 class part1_topo(Topo):
-    def build(self):
-        pass
+
+    def __init__( self ):
+        "Custom topology"
+
+        # Initialize topology
+        Topo.__init__( self )
+
         switch1 = self.addSwitch('s1')
         host1 = self.addHost('h1')
         host2 = self.addHost('h2')
@@ -18,8 +23,21 @@ class part1_topo(Topo):
         self.addLink(host2,switch1)
         self.addLink(host3,switch1)
         self.addLink(host4,switch1)
-
+        
 topos = { 'part1': ( lambda: part1_topo() ) }
+
+    # def build(self):
+    #     pass
+    #     switch1 = self.addSwitch('s1')
+    #     host1 = self.addHost('h1')
+    #     host2 = self.addHost('h2')
+    #     host3 = self.addHost('h3')
+    #     host4 = self.addHost('h4')
+    #     self.addLink(host1,switch1)
+    #     self.addLink(host2,switch1)
+    #     self.addLink(host3,switch1)
+    #     self.addLink(host4,switch1)
+
 # topos = {"part1": part1_topo}
 
 # if __name__ == "__main__":
