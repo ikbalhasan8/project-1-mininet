786c009610bbe553338e126c5235a38defbedb97
ikbalhasan8 <159504256+ikbalhasan8@users.noreply.github.com>
2024-02-13 16:14:06 +0700

    Update part1.py

--- topos/part1.py
+++ topos/part1.py
@@ -1,25 +1,23 @@
+#!/usr/bin/python3
+
 from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.util import dumpNodeConnections
+from mininet.cli import CLI
 
-class MyTopo( Topo ):
-    "Simple topology of six switches and six hosts"
 
-    def __init__( self ):
-        "Custom topology"
+class MyTopo(Topo):
+    def __init__(self):
+        Topo.__init__(self)
+        s1 = self.addSwitch('s1')
+        h1 = self.addHost('h1')
+        h2 = self.addHost('h2')
+        h3 = self.addHost('h3')
+        h4 = self.addHost('h4')
+        self.addLink(h1,s1)
+        self.addLink(h2,s1)
+        self.addLink(h3,s1)
+        self.addLink(h4,s1)
 
-        # Initialize topology
-        Topo.__init__( self )
-
-        # Add hosts and switches
-        host1 = self.addHost( 'h1' )
-        host2 = self.addHost( 'h2' )
-        host3 = self.addHost( 'h3' )
-        host4 = self.addHost( 'h4' )
-        switch1 = self.addSwitch( 's1' )
-
-        # Add links between SW and Hosts
-        self.addLink( host1, switch1 )
-        self.addLink( host2, switch1 )
-        self.addLink( host3, switch1 )
-        self.addLink( host4, switch1 )
 
 topos = { 'mytopo': ( lambda: MyTopo() ) }
